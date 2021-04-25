da01814bc222ea2c7354de18200afd8c23f0c499
Tiago Sarno <tiagosarno@ste-simemp.com.br>
2021-04-25 08:47:39 -0300

    Implementação da Model Schedule

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = "id";
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_psychologist',
+        'id_patient',
+        'schedule',
+        'new_schedule',
+        'status',
+        'platform',
+        'price',
+        'rating',
+        'rating_note',
+        'public_rating'
+    ];
 }
